5eac6d578fef3ecca8f1ab1d509b23b2727a33dd
Spencehiko <hiko.kufte@gmail.com>
2018-12-07 12:05:03 +0300

    Header

--- main.js
+++ main.js
@@ -1,9 +1,45 @@
 console.log("HEY!");
-$(document).ready(function(){
-    $("#msg").html("<h2>jQuery Hello World</h2>");
-   });
-   $(document).ready(function() {
-    $("#newdiv").on('click',function() {
-      alert("Hello world!");
-    });
+
+$(document).ready(function () {
+  $("#msg").html("<h2>jQuery Hello World</h2>");
+  $("#newdiv").on('click', function () {
+  alert("Hello world!");
+ });
+  $('#home').on('click',()=>{
+    alert('clicked');
+    window.location.href = './haha.html';
+  })
+ 
 });
+
+
+
+/*
+$(document).ready(() => {
+  $('.menu').on('mouseenter', () => {
+    $('.nav-menu').show();
+  })
+  $('.nav-menu').on('mouseleave', () => {
+    $('.nav-menu').hide();
+  })
+  $('.btn').on('mouseenter', () => {
+    $(event.currentTarget).addClass('btn-hover');
+  })
+  $('.btn').on('mouseleave', () => {
+    $(event.currentTarget).removeClass('btn-hover');
+  })
+  $('.postText').on('keyup', () => {
+    let remaining = 140;
+    let post = $(event.currentTarget).val();
+    remaining = remaining - post.length;
+    $('.characters').html(remaining);
+    if(remaining <=0){
+      $('.wordcount').addClass('red');
+    }
+    else {
+      $('.wordcount').removeClass('red');      
+    }
+  })
+  $('.postText').focus();
+}); 
+*/
